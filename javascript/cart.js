b62dbd16df0ba64f48bb98deb80d022b5572188c
wow767t <wow767t@gmail.com>
2022-02-21 14:06:38 +0800

    first

---
+++ javascript/cart.js
@@ -0,0 +1,239 @@
+// import { createApp } from "https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.26/vue.esm-browser.min.js";
+import 'https://unpkg.com/mitt/dist/mitt.umd.js'; // mitt
+const emitter = mitt();
+
+
+const apiUrl = 'https://vue3-course-api.hexschool.io/v2/'
+const apiPath = 'scott'
+
+Object.keys(VeeValidateRules).forEach(rule => {
+    if (rule !== 'default') {
+        VeeValidate.defineRule(rule, VeeValidateRules[rule]);
+    }
+});
+
+VeeValidateI18n.loadLocaleFromURL('./zh_TW.json');
+
+// Activate the locale
+VeeValidate.configure({
+    generateMessage: VeeValidateI18n.localize('zh_TW'),
+    validateOnInput: true, // 調整為輸入字元立即進行驗證 
+});
+
+const app = Vue.createApp({
+    data() {
+        return {
+            cart: {},
+            products: [],
+            targetId: '',
+            isLoading: '',
+            formInfo: {
+                "user": {
+                    "name": "",
+                    "email": "",
+                    "tel": "",
+                    "address": ""
+                },
+                "message": ""
+            }
+        }
+    },
+    methods: {
+        // get products data
+        getProducts() {
+            const url = `https://vue3-course-api.hexschool.io/v2/api/scott/products/all`
+            axios.get(url)
+                .then(res => {
+                    // console.log(res)
+                    this.products = res.data.products
+                })
+                .catch(err => {
+                    console.error(err);
+                })
+        },
+        getCart() {
+            const url = `https://vue3-course-api.hexschool.io/v2/api/scott/cart`
+            axios.get(url)
+                .then(res => {
+                    // console.log(res)
+                    this.cart = res.data.data.carts
+                })
+                .catch(err => {
+                    console.error(err);
+                })
+        },
+        addToCart(id, qty = 1) {
+            console.log(id) //product id
+            // https://vue3-course-api.hexschool.io/v2/api/scott/cart
+            const url = `https://vue3-course-api.hexschool.io/v2/api/scott/cart`
+            const data = {
+                "data": {
+                    "product_id": id,
+                    "qty": qty
+                }
+            }
+            this.isLoading = id
+            axios.post(url, data)
+                .then(res => {
+                    // console.log('axios',id)
+
+                    console.log('add to cart', res)
+                    this.getCart()
+                    this.isLoading = ''
+                    alert(res.data.message)
+                })
+                .catch(err => {
+                    console.error(err);
+                })
+
+        },
+        updateCartItem(item) {
+            const url = `https://vue3-course-api.hexschool.io/v2/api/scott/cart/${item.id}`
+            const data = {
+                "data": {
+                    "product_id": item.product.id,
+                    "qty": item.qty
+                }
+            }
+            // this.isLoading = id
+            axios.put(url, data)
+                .then(res => {
+                    // this.getCart()
+                    // this.isLoading = ''
+                    console.log(res.data.message)
+                })
+                .catch(err => {
+                    console.error(err);
+                })
+        },
+
+        openProductModal(id) {
+            // console.log(this.$refs.userProductModal)
+            this.targetId = id
+            this.$refs.userProductModal.openProductModal()
+        },
+        removeCartItem(id) {
+            // https://vue3-course-api.hexschool.io/v2/api/scott/cart/%22-Mw51OkPVZEHzplix6y1%22
+            // console.log('remove', id)
+            this.isLoading = id
+            axios.delete(`https://vue3-course-api.hexschool.io/v2/api/scott/cart/${id}`)
+                .then(res => {
+                    console.log(res)
+                    alert(res.data.message)
+                    this.getCart()
+                    this.isLoading = ''
+                })
+                .catch(err => {
+                    console.error(err);
+                })
+        },
+        clearCart() {
+            axios.delete(`https://vue3-course-api.hexschool.io/v2/api/scott/carts`)
+                .then(res => {
+                    console.log(res)
+                    alert(res.data.message)
+                    this.getCart()
+                })
+                .catch(err => {
+                    console.error(err);
+                })
+        },
+        // 送出訂單
+        submitOrder() {
+            console.log('submitOrder')
+            const data = {
+                "data": {
+                    "user": {
+                        "name": "test",
+                        "email": "test@gmail.com",
+                        "tel": "0912346768",
+                        "address": "kaohsiung"
+                    },
+                    "message": "這是留言"
+                }
+            }
+            //           https://vue3-course-api.hexschool.io/v2/api/scott/order
+            const url = `https://vue3-course-api.hexschool.io/v2/api/scott/order`
+            axios.post(url,data)
+            .then(res => {
+                console.log(res)
+                alert(res.data.message)
+                this.cart = {}
+            })
+            .catch(err => {
+                console.error(err); 
+            })
+
+        },
+        // 電話格式驗證
+        isPhone(value) {
+            const phoneNumber = /^(09)[0-9]{8}$/
+            return phoneNumber.test(value) ? true : '需要正確的電話號碼'
+        }
+
+    },
+    mounted() {
+        this.getProducts()
+        this.getCart()
+
+
+    }
+});
+
+
+
+app.component('userProductModal', {
+    props: ['id'],
+    watch: {
+        id() {
+            this.innerId = this.id
+            this.getProduct()
+        }
+    },
+    data() {
+        return {
+            productModal: '',
+            product: {},
+            innerId: '',
+            qty: null
+        }
+    },
+    template: '#userProductModal',
+    methods: {
+        openProductModal() {
+            this.productModal.show()
+
+        },
+        // get target product
+        getProduct() {
+            // https://vue3-course-api.hexschool.io/v2/api/scott/product/-MuA5NCU-x5SPj-J8RJT
+            // https://vue3-course-api.hexschool.io/v2/api/scott/product/-MuA5NCU-x5SPj-J8RJT
+
+            const url = `https://vue3-course-api.hexschool.io/v2/api/scott/product/${this.innerId}`
+            axios.get(url)
+                .then(res => {
+                    console.log(res)
+                    this.product = res.data.product
+                })
+                .catch(err => {
+                    console.error(err);
+                })
+        },
+        innerAdd() {
+            this.$emit('add-to-cart', this.innerId, this.qty)
+            this.productModal.hide()
+        }
+
+
+    },
+    mounted() {
+        this.productModal = new bootstrap.Modal(this.$refs.modal)
+    }
+});
+
+
+app.component('VForm', VeeValidate.Form);
+app.component('VField', VeeValidate.Field);
+app.component('ErrorMessage', VeeValidate.ErrorMessage);
+
+app.mount('#app')
